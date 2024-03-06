99e4e3ff82f12d731b0befc87af48e292ab9f4f2
Ellie Zheng <ellie.zheng01@sap.com>
2024-03-06 13:21:09 +0800

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     os.environ["OPENAI_API_TYPE"] = "azure"
     os.environ["OPENAI_API_VERSION"] = "2023-05-15"
     os.environ["OPENAI_API_BASE"] = "https://pvg-azure-openai-uk-south.openai.azure.com/openai"
-    env_path = os.getenv("HOME") + "/.env"
+    env_path =  "/.env"
     load_dotenv(dotenv_path=env_path, verbose=True) 
 
     st.title(env_path)
